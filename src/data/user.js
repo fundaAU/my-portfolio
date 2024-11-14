dac4eb264dde9e86eb9f025e088f059b083cf5ef
fundaAU <fundayumuk@gmail.com>
2024-11-14 18:09:34 +1100

    first commit

---
+++ src/data/user.js
@@ -0,0 +1,82 @@
+const INFO = {
+	main: {
+		title: "Reactfolio by Funda Yumuk",
+		name: "Funda Yumuk",
+		email: "fundayumuk@gmail.com",
+		logo: "../logo.png",
+	},
+
+	socials: {
+				github: "https://github.com/fundaAU",
+				instagram: "https://www.instagram.com/fundayumuk/?hl=tr",
+		
+		facebook: "https://www.facebook.com/people/Funda-Yumuk/pfbid02egKEMB8rCcisUE3L8PectrUaKPUauWQ7h87nKxxAVCJKFbJoYC9PUAgzguwmAsJNl/",
+	},
+
+	homepage: {
+		title: "Statistician and Web Developer, Data Analysis and Digital Solutions Expert.",
+		description:
+			"Over the years, I've developed numerous projects and products tailored to meet user needs. These projects are not only personal achievements but also solutions that add value to the workflows of communities and companies. Through these projects, I achieved a 35% increase in user engagement, 50% faster processing times, and developed projects that have been used by over 1,000 active users. Many of these projects have been made open-source, allowing other developers to contribute. These efforts have significantly contributed to both my growth and the growth of the community. If you're interested in any of these projects, feel free to explore the code and contribute with any improvements or new features you might have in mind. I firmly believe that collaboration and teamwork are essential for creating innovative solutions and fostering professional development. That's why I'm always open to new ideas and feedback.",
+	},
+
+	about: {
+		title: "I’m Funda Y. Based in Melbourne, shaping the future with innovative digital solutions.",
+		description:
+			"Over the years, I’ve had the opportunity to work on a variety of projects, including statistical analyses and data-driven solutions, and I’m proud of the progress I’ve made. Many of these projects are open-source and available for others to explore and contribute to. If you're interested in any of my work, including statistical models and web solutions, feel free to explore the code and suggest any improvements or enhancements you might have in mind. Collaboration is a great way to learn and grow, and I’m always open to new ideas and feedback.",
+	},
+
+	articles: {
+		title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
+		description:
+			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+	},
+
+	projects: [
+		{
+			title: "Project 1",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 2",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 3",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 4",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 5",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+	],
+};
+
+export default INFO;
